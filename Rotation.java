0befef3fef407135e4aa67c716c3d327e447abad
Kelin Christi <christi.kelin20@gmail.com>
2016-03-29 15:31:03 -0700

    Checking for rotation algorithm solved

---
+++ Rotation.java
@@ -0,0 +1,32 @@
+public class Rotation {
+	//Now that we pass in 2 strings, one that is concatenated by itself, and one to check, we run the indexOf method and return a bool.
+	public static boolean isSubstring(String big, String small) {
+		if (big.indexOf(small) >= 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	//First check to make sure that elements in both strings are the same length.
+	public static boolean isRotation(String s1, String s2) {
+	    int len = s1.length();
+	    /* check that s1 and s2 are equal length and not empty */
+	    if (len == s2.length() && len > 0) { 
+	    	/* concatenate s1 and s1 within new buffer */
+	    	String s1s1 = s1 + s1;
+	    	return isSubstring(s1s1, s2);
+	    }
+	    return false;
+	}
+	
+	public static void main(String[] args) {
+		String[][] pairs = {{"pear", "rpea"}, {"bottle", "bottle"}, {"obscura", "curaobs"}};
+		for (String[] pair : pairs) {
+			String word1 = pair[0];
+			String word2 = pair[1];
+			boolean is_rotation = isRotation(word1, word2);
+			System.out.println(word1 + ", " + word2 + ": " + is_rotation);
+		}
+	}
+
+}
